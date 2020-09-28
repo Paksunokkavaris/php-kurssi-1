f41f917e7800d92e2337fb5ba73e18855cc6c5b0
Paksunokkavaris <72013411+Paksunokkavaris@users.noreply.github.com>
2020-09-28 13:04:13 +0300

    Update index.php
    palautusta kokeilee

--- 1-2/index.php
+++ 1-2/index.php
@@ -1,1 +1,24 @@
 <?php
+
+	// luo muutujat...
+
+
+	//tunniste = sisältö
+	//string = merkkijono
+
+$letters = "Hello";
+
+	//tunniste = sisältö
+	//integer  = tasaluku
+
+$number = 589;
+
+	//tunniste = sisältö
+	//float = desimaali pisteellä ei pilkulla 
+
+$decimal = 50.95;
+
+	//tunniste = sisältö
+	//table = array() tai pelkkä []
+
+$table = [ 'A','B','C'];
