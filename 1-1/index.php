79316f621c1e8e82d2e3f115fe391da45abe9e4c
Paksunokkavaris <72013411+Paksunokkavaris@users.noreply.github.com>
2020-09-28 12:49:01 +0300

    Update index.php

--- 1-1/index.php
+++ 1-1/index.php
@@ -1,1 +1,13 @@
+
 <?php
+
+//echo = tulostus käsky   "" = teksti jonka haluaa tulostaa...  
+//tulostu käskyinä toimii myös print & printf (printf )
+
+echo "Hello World!";
+
+// tai tavalla 
+ 
+$tervehdys = "Hello World!";
+print tervehdys;
+
